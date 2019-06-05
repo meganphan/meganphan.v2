f45186a1c401db734dfb76f122aef80cc66d9891
MeganPhan <kimhoang7994@gmail.com>
2019-06-05 00:06:20 -0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <p>Hi, my name is</p>
-    <h1>Megan Phan.</h1>
-    <p>I am currently building this website.</p>
+    <div>
+    <div style="width:100%;height:0;padding-bottom:75%;position:relative;"><iframe src="https://giphy.com/embed/1XgIXQEzBu6ZWappVu" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/gif-this-hammer-under-construction-hammered-1XgIXQEzBu6ZWappVu">via GIPHY</a></p>
     <p>Check out the previous website <a href="https://meganphan.github.io/meganphan.v1/">here</a></p>
+    </div>
   </Layout>
 )
 
