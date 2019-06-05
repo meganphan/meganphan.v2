d2b41d36a101a88f0839f1e75f14c81dfe6e23a2
MeganPhan <kimhoang7994@gmail.com>
2019-06-05 00:11:18 -0500

    change

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,6 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout >
   <SEO title = "Home"/>
-  <div style = "width:100%;height:0;padding-bottom:75%;position:relative;"> <iframe src = "https://giphy.com/embed/1XgIXQEzBu6ZWappVu"
-  width = "100%"
-  height = "100%"
-  style = "position:absolute"
-  frameBorder = "0"
-  class = "giphy-embed"
-  allowFullScreen> </iframe></div>
   
   </Layout>
 )
