f945ae897fbe86d3da7de37a901c3df52b0a03e7
MeganPhan <kimhoang7994@gmail.com>
2019-06-11 16:37:03 -0500

    ddd

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import * as config from "../components/config/index"
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
