8f34f608a9f060e2ab74f4008a20ae300fc9c219
MeganPhan <kimhoang7994@gmail.com>
2019-06-17 13:09:27 -0500

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,28 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import {Link} from "gatsby"
+import styled from "@emotion/styled"
+import Sad from "../components/icons/sad"
+
+const Container = styled.div`
+    text-align: center;
+    height: 60vh;
+    margin-top: 20vh;
+    & h1{
+      font-size: 80px;
+    }
+`
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+    <h1>404</h1>
+    <Sad />
+    <p>Oops, it looks like the page you're trying to find is not available.</p>
+    <Link to={"/"}>Go back to Home</Link>
+    </Container>
   </Layout>
 )
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,7 @@ import React from "react"
 import styled from "@emotion/styled"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import {Link} from "gatsby"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faHeart } from "@fortawesome/free-solid-svg-icons";
-// import IconHeart from "../components/icons/heart"
+import avatar from "../images/avatar.jpg"
 
 const Content = styled.div`
   margin: 0 auto;
@@ -28,17 +25,50 @@ const Content = styled.div`
        }
     }
 `
+const FlexContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+const ContentContainer = styled.div`
+    & p{
+      text-align: justify;
+    }
+    & h1{
+      text-align: center;
+      animation: 
+    }
+
+    & h1:after{
+      content: "Hello!";
+      display: none;
+    }
+`
+const Avatar = styled.img`
+  position: relative;
+  mix-blend-mode: multiply;
+  filter: grayscale(0%) contrast(1);
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin-bottom: 10px;x
+`;
 
 const About = () => (
   <Layout>
     <SEO title="About" />
     <Content>
         {/* eslint-disable */}
+        <FlexContainer>
+        <Avatar src={avatar} alt="Logo" />
+        </FlexContainer>
+        <ContentContainer>
         <h1>Annyeonghaseyo!</h1>
-        {/* <p>I am Hoang Phan, and also go by Megan. I am a forth-year Computer Science student at <a href="https://computerscience.tcu.edu/" target="_blank">Texas Christian University.</a></p> */}
-
+        <p>I am Hoang, but call me Megan because "Hoang Phan" sounds like a 18-year-old boy, which I am not. </p>
+        <p>When I am not in front of a computer, I'd play with my cat Michio, cook some delicious Vietnamese dishes, decorate my room, or spend the whole afternoon cleaning my apartment.</p>
+        </ContentContainer>
     </Content>
   </Layout>
 )
+
 
 export default About
