3faa6d21c8be680dc404a105221b74f7c874ca31
MeganPhan <kimhoang7994@gmail.com>
2019-07-07 13:30:52 -0500

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,39 +29,39 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `
-  ).then(result => {
-    if(result.errors) {
-      return Promise.reject(result.errors)
-    }
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        slug: node.fields.slug,
-        context: {},
-      })
-    })
-  })
-}
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`);
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `
+//   ).then(result => {
+//     if(result.errors) {
+//       return Promise.reject(result.errors)
+//     }
+//     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: blogPostTemplate,
+//         slug: node.fields.slug,
+//         context: {},
+//       })
+//     })
+//   })
+// }
 
 
 exports.onCreateWebpackConfig = ({ actions }) => {
@@ -69,10 +69,10 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     resolve: {
       alias: {
         '@components': path.resolve(__dirname, 'src/components'),
-        '@content': path.resolve(__dirname, 'src/content'),
+        // '@content': path.resolve(__dirname, 'src/content'),
         '@images': path.resolve(__dirname, 'src/images'),
         '@pages': path.resolve(__dirname, 'src/pages'),
-        '@templates': path.resolve(__dirname, 'src/templates'),
+        // '@templates': path.resolve(__dirname, 'src/templates'),
       },
     },
   });
