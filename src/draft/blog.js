b288f637cc71318b04bff77b7c5a5d2b8a30c3e2
MeganPhan <kimhoang7994@gmail.com>
2019-07-07 13:32:39 -0500

    update

--- src/draft/blog.js
+++ src/draft/blog.js
@@ -1,97 +1,97 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import { css } from "@emotion/core"
-import styled from "@emotion/styled"
+// import React from "react"
+// import { Link, graphql } from "gatsby"
+// import { css } from "@emotion/core"
+// import styled from "@emotion/styled"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
 
-const Content = styled.div`
-  margin: 0 auto;
-  max-width: 860px;
-  padding: 1.45rem 1.0875rem;
-  animation: 1s ease-in-out 0s 1 opacity forwards;  
-  opacity: 1;
-`
+// const Content = styled.div`
+//   margin: 0 auto;
+//   max-width: 860px;
+//   padding: 1.45rem 1.0875rem;
+//   animation: 1s ease-in-out 0s 1 opacity forwards;  
+//   opacity: 1;
+// `
 
-const ArticleDate = styled.h5`
-  display: inline;
-  color: #606060;
-  margin-bottom: 10px;
-`
+// const ArticleDate = styled.h5`
+//   display: inline;
+//   color: #606060;
+//   margin-bottom: 10px;
+// `
 
-const MarkerHeader = styled.h3`
-  display: inline;
-  margin-bottom: 10px;
-  color:  hsla(0, 0%, 0%, 0.8);
-  &:hover{
-    color: rgb(23, 151, 151);
-  }
-`
+// const MarkerHeader = styled.h3`
+//   display: inline;
+//   margin-bottom: 10px;
+//   color:  hsla(0, 0%, 0%, 0.8);
+//   &:hover{
+//     color: rgb(23, 151, 151);
+//   }
+// `
 
-const ReadingTime = styled.h5`
-  display: inline;
-  color: #606060;
-  margin-bottom: 10px;
-`
+// const ReadingTime = styled.h5`
+//   display: inline;
+//   color: #606060;
+//   margin-bottom: 10px;
+// `
 
-const IndexPage = ({ data }) => {
-  return (
-    <Layout>
-    <SEO title="Blog" keywords={["blog","gatsby", "application", "react", "megan phan", "hoang phan", "frontend", "web developer", "texas christian university", "software engineer", "portfolio"]} />
-      <Content>
-        <h1>Blog</h1>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link
-              to={node.frontmatter.path}
-              css={css`
-                text-decoration: none;
-                color: inherit;
-              `}
-            >
-              <MarkerHeader>{node.frontmatter.title} </MarkerHeader>
-              <div>
-                <ArticleDate>{node.frontmatter.date}</ArticleDate>
-                <ReadingTime> - {node.fields.readingTime.text}</ReadingTime>
-              </div>
-              <p>{node.excerpt}</p>
-            </Link>
-          </div>
-        ))}
-      </Content>
-    </Layout>
-  )
-}
+// const IndexPage = ({ data }) => {
+//   return (
+//     <Layout>
+//     <SEO title="Blog" keywords={["blog","gatsby", "application", "react", "megan phan", "hoang phan", "frontend", "web developer", "texas christian university", "software engineer", "portfolio"]} />
+//       <Content>
+//         <h1>Blog</h1>
+//         {data.allMarkdownRemark.edges.map(({ node }) => (
+//           <div key={node.id}>
+//             <Link
+//               to={node.frontmatter.path}
+//               css={css`
+//                 text-decoration: none;
+//                 color: inherit;
+//               `}
+//             >
+//               <MarkerHeader>{node.frontmatter.title} </MarkerHeader>
+//               <div>
+//                 <ArticleDate>{node.frontmatter.date}</ArticleDate>
+//                 <ReadingTime> - {node.fields.readingTime.text}</ReadingTime>
+//               </div>
+//               <p>{node.excerpt}</p>
+//             </Link>
+//           </div>
+//         ))}
+//       </Content>
+//     </Layout>
+//   )
+// }
 
-export default IndexPage
+// export default IndexPage
 
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            path
-          }
-          fields {
-            slug
-            readingTime {
-              text
-            }
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+//       totalCount
+//       edges {
+//         node {
+//           id
+//           frontmatter {
+//             title
+//             date(formatString: "DD MMMM, YYYY")
+//             path
+//           }
+//           fields {
+//             slug
+//             readingTime {
+//               text
+//             }
+//           }
+//           excerpt
+//         }
+//       }
+//     }
+//   }
+// `
